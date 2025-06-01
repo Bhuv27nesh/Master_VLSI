dccc5d936dd728277df0e0ebfd8a45130ecc6239
Bhuv27nesh <27bhuvanesh5@gmail.com>
2025-06-01 19:08:40 +0530

    Update ASSIGNMENT_1.v

--- ASSIGNMENT_1/ASSIGNMENT_1.v
+++ ASSIGNMENT_1/ASSIGNMENT_1.v
@@ -10,12 +10,12 @@ output reg y
 );
 
 always@(*)begin
-	case(sel)
-		2'b00:y<=I[0];
-		2'b01:y<=I[1];
-		2'b10:y<=I[2];
-		2'b11:y<=I[3];
-		default:y<=y;
+  case(select)
+		2'b00:y=I[0];
+		2'b01:y=I[1];
+		2'b10:y=I[2];
+		2'b11:y=I[3];
+		default:y=1'bx;
 	endcase
 end
 
